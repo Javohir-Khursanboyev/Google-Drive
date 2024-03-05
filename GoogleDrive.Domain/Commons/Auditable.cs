69856f56480082a51309473836118dfa9f0cd7dc
Javohir <xursanboyevj26@gmail.com>
2024-03-06 00:35:03 +0500

    Added GoogleDrive

---
+++ GoogleDrive.Domain/Commons/Auditable.cs
@@ -0,0 +1,10 @@
+﻿namespace GoogleDrive.Domain.Commons;
+
+public class Auditable
+{
+    public long Id { get; set; }    
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime DeletedAt { get; set; }
+    public bool IsDeleted { get; set; }
+}
