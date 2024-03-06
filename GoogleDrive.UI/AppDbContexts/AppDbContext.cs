45cd031906e08f3c9d96ce05cfb3c6ccb8515dc1
Javohir <xursanboyevj26@gmail.com>
2024-03-06 16:42:57 +0500

    Added dbContext

---
+++ GoogleDrive.UI/AppDbContexts/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GoogleDrive.Models.UserModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+
+namespace GoogleDrive.UI.AppDbContexts;
+
+public class AppDbContext:DbContext
+{
+    DbSet<UserModel> users { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Server=dpg-cngviua0si5c73blm71g-a.oregon-postgres.render.com;Database=airportdb;" +
+     "User Id=javohir03;Password=cIKLeyAx4NGBPfMujI252EpIkCSsySPq;");
+    }
+}
