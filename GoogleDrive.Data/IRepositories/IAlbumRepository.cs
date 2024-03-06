cd36644fe9661b3bef0dc1ae08102fd6dab333d4
Javohir <xursanboyevj26@gmail.com>
2024-03-06 23:13:00 +0500

    Added AlbumRepository and AlbumSeervice

---
+++ GoogleDrive.Data/IRepositories/IAlbumRepository.cs
@@ -0,0 +1,12 @@
+﻿using GoogleDrive.Domain.Entities.Albums;
+using GoogleDrive.Domain.Entities.Users;
+
+namespace GoogleDrive.Data.IRepositories;
+
+public interface IAlbumRepository
+{
+    Task<Album> InsertAsync(Album album);
+    Task<Album> UpdateAsync(long id, Album album);
+    Task<bool> DeleteAsync(long id);
+    Task<IEnumerable<Album>> GetAllAsync();
+}
