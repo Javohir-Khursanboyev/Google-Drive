c99fa636039014dd2060de3d2f1ac3cd36e568df
xudaykulov-xamidullo <xudaykulov123@gmail.com>
2024-03-06 10:46:56 +0500

    Inhertence from Abstract Class Auditable

--- GoogleDrive.Models/AlbumModel.cs
+++ GoogleDrive.Models/AlbumModel.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GoogleDrive.Domain.Commons;
 
 namespace GoogleDrive.Models;
 
-public class AlbumModel
+public class AlbumModel : Auditable
 {
-    public long Id { get; set; }
     public string Name { get; set; }
     public long UserId { get; set; }
 }
--- GoogleDrive.Models/UserModel.cs
+++ GoogleDrive.Models/UserModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GoogleDrive.Domain.Commons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +7,12 @@ using System.Threading.Tasks;
 
 namespace GoogleDrive.Models;
 
-public class UserModel
+public class UserModel : Auditable
 {
-    public long Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
-    public bool Privact { get; set; } = false;
+    public bool Privacy { get; set; } = false;
 }
