0f61d58499305929eff623cf9409057e15ddbb08
Javohir <xursanboyevj26@gmail.com>
2024-03-06 18:35:20 +0500

    Added Repsotory

---
+++ GoogleDrive.Data/IRepositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using GoogleDrive.Models.UserModels;
+
+namespace GoogleDrive.Data.IRepositories;
+
+public interface IUserRepository
+{
+    Task<UserModel> InsertAsync(UserModel user);
+    Task<UserModel> UpdateAsync(long id, UserModel user);   
+    Task<bool> DeleteAsync(long id);
+    Task<UserModel> GetAllAsync();
+}
---
+++ GoogleDrive.Data/Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using GoogleDrive.Data.IRepositories;
+using GoogleDrive.Models.UserModels;
+using GoogleDrive.UI.AppDbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoogleDrive.Data.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    public readonly AppDbContext appDb;
+    private readonly DbSet<UserModel>users;
+    public UserRepository(AppDbContext appDb)
+    {
+        this.appDb = appDb;
+    }
+    public Task<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserModel> GetAllAsync()
+    {
+        
+        throw new NotImplementedException();
+    }
+
+    public async Task<UserModel> InsertAsync(UserModel user)
+    {
+        
+        throw new NotImplementedException();
+    }
+
+    public Task<UserModel> UpdateAsync(long id, UserModel user)
+    {
+        throw new NotImplementedException();
+    }
+}
