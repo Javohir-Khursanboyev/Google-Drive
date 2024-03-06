bc37c5856dd956b322aecfb9b1fd63b9feb3343d
xudaykulov-xamidullo <xudaykulov123@gmail.com>
2024-03-06 10:35:30 +0500

    The Default Models Is Created

---
+++ GoogleDrive.Models/AlbumModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleDrive.Models;
+
+public class AlbumModel
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public long UserId { get; set; }
+}
---
+++ GoogleDrive.Models/ContentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleDrive.Models;
+
+public class ContentModel
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime SetTime { get; set; }
+    public long UserId { get; set; }
+    public long AlbumId { get; set; }
+}
