f9ce07dd93a042bec0110f87797063fec2bee66a
xudaykulov-xamidullo <xudaykulov123@gmail.com>
2024-03-06 15:14:18 +0500

    Changed Model Folders

---
+++ GoogleDrive.Domain/Enteties/AlbumModels/AlbumCreationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleDrive.Models.AlbumModels;
+
+public class AlbumCreationModel
+{
+    public string Name { get; set; }
+    public long UserId { get; set; }
+}
---
+++ GoogleDrive.Domain/Enteties/ContentModels/ContentModel.cs
@@ -0,0 +1,16 @@
+﻿using GoogleDrive.Domain.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleDrive.Models.ContentModels;
+
+public class ContentModel : Auditable
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public long UserId { get; set; }
+    public long AlbumId { get; set; }
+}
