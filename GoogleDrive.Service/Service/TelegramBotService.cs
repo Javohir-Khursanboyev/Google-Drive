1d16603df9d59d3a2b6371044cf911f2bebc5232
xudaykulov-xamidullo <xudaykulov123@gmail.com>
2024-03-07 12:51:32 +0500

    Created TelegramBotService In GoogleDrive.Service.Service

---
+++ GoogleDrive.Service/Service/TelegramBotService.cs
@@ -0,0 +1,18 @@
+﻿using GoogleDrive.Data.AppDbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace GoogleDrive.Service.Service;
+
+public class TelegramBotService
+{
+    TelegramBotClient botClient;
+    public TelegramBotService(AppDbContext appDbContext)
+    {
+        this.botClient = new TelegramBotClient("6791483512:AAHXmxeGLbqc5lR4JnrX2qzzLyMrvF6C-gM");
+    }
+}
---
+++ GoogleDrive.TelegramBot/TelegramBotService/TelegramService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoogleDrive.Data;
+
+namespace GoogleDrive.TelegramBot.TelegramBotService;
+
+public class TelegramService
+{
+    public TelegramService()
+    {
+        var appDb = new AppDbContext();
+    }
+}
